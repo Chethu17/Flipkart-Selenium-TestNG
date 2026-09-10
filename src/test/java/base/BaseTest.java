c4191eddaeb9f9d572bee1638679cece9a7f799c
chethan <mchethan621@gmail.com>
2026-09-10 19:50:10 +0530

    headless mode

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -27,10 +27,10 @@ public class BaseTest {
     public void setup() {
         
     	ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless=new");
+        options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        //options.addArguments("--disable-gpu");
+        options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
 
         options.setPageLoadStrategy(PageLoadStrategy.EAGER);
